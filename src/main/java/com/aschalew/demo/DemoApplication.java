ba135861e98a632515a633091268a9cfdd7b67de
aschalew-at-585442991009 <aschalewat@yahoo.co.uk>
2023-08-12 07:56:36 +0100

    Updated Sonar

--- src/main/java/com/aschalew/demo/DemoApplication.java
+++ src/main/java/com/aschalew/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("This is a test printtttt");
+		System.out.println("This is a test printt");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
