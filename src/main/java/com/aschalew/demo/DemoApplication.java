0ad20035530858461cb6ac63201f764bac2f2f51
aschalew-at-585442991009 <aschalewat@yahoo.co.uk>
2023-08-12 13:30:40 +0100

    Updated Sonar

--- src/main/java/com/aschalew/demo/DemoApplication.java
+++ src/main/java/com/aschalew/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 
-		System.out.println("This is a test printttt");
+		System.out.println("This is a test print");
 
 		SpringApplication.run(DemoApplication.class, args);
 	}
