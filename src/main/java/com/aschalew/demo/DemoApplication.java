061d54deee54ecd4d19b2b20283cc93a25267562
aschalew-at-585442991009 <aschalewat@yahoo.co.uk>
2023-11-15 14:36:33 +0000

    Updated main

--- src/main/java/com/aschalew/demo/DemoApplication.java
+++ src/main/java/com/aschalew/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("This is a test printfghfii");
+		System.out.println("This is a test printfghi");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
